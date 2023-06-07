fcf4b2d3a29cca6b6fbcd73ea6a7892502ba6145
Caio Maciel <caiolmaciell@gmail.com>
2023-06-07 01:25:22 -0300

    feature(AppService): Adding jest unitary tests

---
+++ src/app.service.spec.ts
@@ -0,0 +1,23 @@
+import { AppService } from './app.service';
+
+describe('AppService', () => {
+  let appService: AppService;
+
+  describe('findByJson', () => {
+    it('should return the path of the created file', async () => {
+      return;
+    });
+  });
+
+  describe('findByCsv', () => {
+    it('should return the path of the created file', async () => {
+      return;
+    });
+  });
+
+  describe('alocateStoreage', () => {
+    it('should return a valid file path', async () => {
+      return;
+    });
+  });
+});
